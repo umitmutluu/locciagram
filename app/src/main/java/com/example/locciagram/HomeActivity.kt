782a12f9d98b6199ee88e0b2821bf4b89e8a1167
umitmutluu <umitmutlurize@gmail.com>
2022-05-02 15:56:47 +0300

    commit second -> some images added

--- app/src/main/java/com/example/locciagram/HomeActivity.kt
+++ app/src/main/java/com/example/locciagram/HomeActivity.kt
@@ -3,9 +3,9 @@ package com.example.locciagram
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
